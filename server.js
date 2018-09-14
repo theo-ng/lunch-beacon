ba956cee5b24d860966f45b19ba23dd83ff8a51d
Theodore Ng <theo.ng@bananatag.com>
2018-09-14 10:48:46 -0700

    add get route

--- server.js
+++ server.js
@@ -17,18 +17,25 @@ const port = process.env.PORT || 5000;
 admin.initializeApp(config);
 
 const db = admin.firestore();
-db.collection('restaurants').get()
-    .then((snapshot) => {
-        snapshot.forEach((doc) => {
-            console.log(doc.id, '=>', doc.data());
+_getRestaurants = () => {
+    const res = {};
+    db.collection('restaurants').get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                res[doc.id] = doc.data();
+            });
+        })
+        .catch((err) => {
+            console.log('Error getting docs', err);
         });
-    })
-    .catch((err) => {
-        console.log('Error getting docs', err);
-    });
+}
+
+app.get('/restaurants', (req, res) => {
+    res.send(_getRestaurants());
+});
 
 app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+    res.send({ express: 'Hello From Express' });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
